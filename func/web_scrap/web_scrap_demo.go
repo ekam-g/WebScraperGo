6079d6ef6b112c90933815fede3c5e787840324f
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-08-09 17:59:45 -0400

    some changes

---
+++ func/web_scrap/web_scrap_demo.go
@@ -0,0 +1,26 @@
+package web_scrap
+
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+)
+
+type Scrape struct {
+}
+
+func (Scrape) Demo() (value []interface{}) {
+	c := colly.NewCollector()
+	c.OnHTML("table#customers", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
+			value = append(value,
+				[]string{
+					el.ChildText("td:nth-child(1)"),
+					el.ChildText("td:nth-child(2)"),
+					el.ChildText("td:nth-child(3)"),
+				})
+		})
+		fmt.Println("Scrapping Complete")
+	})
+	c.Visit("https://www.w3schools.com/html/html_tables.asp")
+	return value
+}
--- main..go
+++ main..go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"github.com/gocolly/colly"
-)
+import "WebScraperGo/func/web_scrap"
 
 func main() {
-	c := colly.NewCollector()
-	c.OnHTML("table#customers", func(e *colly.HTMLElement) {
-		e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
-			fmt.Println([]string{
-				el.ChildText("td:nth-child(1)"),
-				el.ChildText("td:nth-child(2)"),
-				el.ChildText("td:nth-child(3)"),
-			})
-		})
-		fmt.Println("Scrapping Complete")
-	})
-	c.Visit("https://www.w3schools.com/html/html_tables.asp")
+	Demo := web_scrap.Scrape.Demo()
 }
